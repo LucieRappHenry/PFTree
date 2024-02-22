9f5213999a726feee7f39fd1f9f8b912c2593e38
LucieRappHenry <122989011+LucieRappHenry@users.noreply.github.com>
2024-02-22 11:13:40 +0100

    Update compil_pf.sh
    directory corrections

--- compil_pf.sh
+++ compil_pf.sh
@@ -10,11 +10,11 @@ export CMAKE_NETCDF_SILO_DIR=$INSTALL_DIR/cmake_netcdf_silo
 # nom de la version parflow 
 export PARFLOW_DIR=$INSTALL_DIR/parflow-240212
 
-export NETCDF_DIR=$INSTALL_DIR/netcdf-parallel-new
+export NETCDF_DIR=$CMAKE_NETCDF_SILO_DIR/netcdf-parallel-new
 export HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/openmpi
 export PATH=$NETCDF_DIR/bin:$PATH
-export PATH=$INSTALL_DIR/cmake-dev/bin:$PATH
-export PARFLOW_SRC=$INSTALL_DIR/parflow
+export PATH=$CMAKE_NETCDF_SILO_DIR/cmake-dev/bin:$PATH
+export PARFLOW_SRC=$INSTALL_DIR/parflow_src
 
 cd $PARFLOW_SRC
 rm -rf build; mkdir build
